e85c9d51134ef63579df123b875756cfa2552848
Maq301 <santbesy@hotmail.com>
2017-04-27 20:03:47 -0300

    Cree dos archivos,main.py y menu.py.En menu.py desarrolle mostrar_modo_de_juego.

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import menu
 menu.mostrar_menu_de_bienvenida()
+menu.mostrar_modo_de_juego()
+
--- menu.py
+++ menu.py
@@ -1,2 +1,9 @@
 def mostrar_menu_de_bienvenida():
         print("Bienvenido a Light out")
+def mostrar_modo_de_juego():
+    print("Elija modo de juego")
+    print("1 - Predeterminado")
+    print("2 - Aleatorio")
+    print("3 - Salir")
+
+
