7bb7d5e6af88d80a6fadb9c2a8c0f921e32d1a2b
Maq301 <santbesy@hotmail.com>
2017-05-02 20:29:07 -0300

    en menu.py hice eleccion de modos de juego y lo subi a git hub.

--- menu.py
+++ menu.py
@@ -5,5 +5,11 @@ def mostrar_modo_de_juego():
     print("1 - Predeterminado")
     print("2 - Aleatorio")
     print("3 - Salir")
+    modoElegido=input()
 
-
+    if (modoElegido == "1"):
+        print("Ha elegido juego en modo predeterminado")
+    elif (modoElegido == "2"):
+        print("Ha elegido juego en modo aleatorio")
+    elif (modoElegido== "3" ):
+        print ("Ha elegido salir")
